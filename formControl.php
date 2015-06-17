93858a52ee85ce1067a276c681bcd117a9dbd079
Admin <sam510@mail.ru>
2015-06-17 19:12:10 +0300

    connect full

--- formControl.php
+++ formControl.php
@@ -3,7 +3,7 @@ require 'database_connection.php';
 $name = $_REQUEST['name'];
 $last_name = $_REQUEST['lastName'];
 $year = $_REQUEST['year'];
-$insert_sql = "INSERT INTO users(first_name, last_name, bd_year) VALUES('{$name}', '{$last_name}', '{$year}')";
-mysql_query($insert_sql)
+$query = "INSERT INTO users(first_name, last_name, bd_year) VALUES('{$name}', '{$last_name}', '{$year}')";
+$link->query($query)
     or die(mysql_error());
  ?>
