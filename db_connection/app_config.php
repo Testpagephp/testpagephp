6c684e4975ca50400336e0d4d45ee8fd30d03ef2
Alexey <alekseymodin@gmail.com>
2015-06-18 15:28:30 +0400

    all users

--- db_connection/app_config.php
+++ db_connection/app_config.php
@@ -3,5 +3,5 @@
 define("DATABASE_HOST", "127.0.0.1");
 define("DATABASE_USERNAME", "root");
 define("DATABASE_PASSWORD", "");
-define("DATABASE_NAME", "test");
+define("DATABASE_NAME", "testpage");
 ?>
